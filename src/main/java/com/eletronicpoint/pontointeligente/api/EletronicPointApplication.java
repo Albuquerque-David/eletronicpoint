ff10e836a6fd40d510e66d34e31835ec9b96bd65
David Albuquerque <davidra_rj@hotmail.com>
2020-03-31 22:40:16 -0300

    First commit

---
+++ src/main/java/com/eletronicpoint/pontointeligente/api/EletronicPointApplication.java
@@ -0,0 +1,13 @@
+package com.eletronicpoint.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletronicPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletronicPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eletronicpoint/pontointeligente/api/EletronicPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eletronicpoint.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EletronicPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
