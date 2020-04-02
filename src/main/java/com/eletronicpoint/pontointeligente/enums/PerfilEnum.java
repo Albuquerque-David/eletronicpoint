ed1508ed2dcc06c59cb3eb0b05be826c4e7d16f8
David Albuquerque <davidra_rj@hotmail.com>
2020-04-02 19:19:41 -0300

    Modelagem inicial das entities

--- src/main/java/com/eletronicpoint/pontointeligente/enums/PerfilEnum.java
+++ src/main/java/com/eletronicpoint/pontointeligente/enums/PerfilEnum.java
@@ -1,13 +1,17 @@
 package com.eletronicpoint.pontointeligente.enums;
 
 public enum PerfilEnum {
-	ROLE_ADMIN("ROLE_ADMIN"),
-	ROLE_USUARIO("ROLE_USUARIO");
+	ROLE_ADMIN("ROLE ADMIN"),
+	ROLE_USUARIO("ROLE USUARIO");
 	
 	private String value;
 	
 	PerfilEnum(String value) 
 	{
 		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
 	}
 }
--- src/main/java/com/eletronicpoint/pontointeligente/enums/TipoEnum.java
+++ src/main/java/com/eletronicpoint/pontointeligente/enums/TipoEnum.java
@@ -1,5 +1,21 @@
 package com.eletronicpoint.pontointeligente.enums;
 
 public enum TipoEnum {
+	INICIO_TRABALHO("INICIO TRABALHO"),
+	TERMINO_TRABALHO("TERMINO TRABALHO"),
+	INICIO_ALMOCO("INICIO ALMOCO"),
+	TERMINO_ALMOCO("TERMINO ALMOCO"),
+	INICIO_PAUSE("INICIO PAUSE"),
+	TERMINO_PAUSE("TERMINO PAUSE");
+	
+	private String value;
+	
+	TipoEnum(String value) 
+	{
+		this.value = value;
+	}
 
+	public String getValue() {
+		return value;
+	}
 }
